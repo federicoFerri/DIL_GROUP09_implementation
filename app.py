ce5ee646f387883cdfa4773ffe65d92ee0bb0bd0
Federico Ferri <federico.ferri@smartcreative.eu>
2021-05-25 12:07:36 +0200

    adding db connection

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+import pymongo
 
 app = Flask(__name__, static_url_path='')
+
+mongodb_uri = 'mongodb+srv://app:Fq0gY35jenP1Dn4s@main.3jwk1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+client = pymongo.MongoClient(mongodb_uri, connect=False)
+main_db = client.main_db
 
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
+
+@app.route('/api/')
+def api_index():
+    main_db.test_collection.find({})
+    return 'Hello world api'
 
 
