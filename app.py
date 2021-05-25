1992c8e539883780349e852e01f20236e7ffb08d
Federico Ferri <federico.ferri@smartcreative.eu>
2021-05-25 11:34:10 +0200

    adding web part and instructions

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return app.send_static_file('index.html')
+
 
 
